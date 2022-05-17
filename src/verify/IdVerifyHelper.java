c8b8e0f77700e760a4753d2a2284c1c6a2c0240e
sunny <sunnyright159@gmail.com>
2022-05-17 17:40:35 +0800

    amd

--- src/verify/IdVerifyHelper.java
+++ src/verify/IdVerifyHelper.java
@@ -1,4 +1,52 @@
 package verify;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class IdVerifyHelper {
+
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int[] letterNums =
+                {10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33};
+        try {
+            String pathname = "src/verify/idList.txt";
+            File filename = new File(pathname);
+            BufferedReader br = new BufferedReader(new FileReader(String.valueOf(filename)));//构造一个BufferedReader类来读取文件
+            String s = "";
+            while ((s = br.readLine()) != null) {
+                System.out.println("====您輸入的身分證字號" + s + "====");
+                if (s.matches("[a-zA-Z]\\d{9}")) {
+                    s = s.toUpperCase();
+                    char letter = s.charAt(0); //取出第一個字母
+                    s = letterNums[letter - 'A'] + s.substring(1);
+                    int total = s.charAt(0) - '0';
+                    if (s.length() > 9) {
+                        for (int i = 1; i < 10; i++) {
+                            total += (s.charAt(i) - '0') * (10 - i); //依序加總
+                        }
+                        //以10減去加總值之個位數後取個位數
+                        int checkNum = (10 - total % 10) % 10;
+
+                        //計算結果和最後一位數比較
+                        if (checkNum == (s.charAt(10) - '0')) {
+                            System.out.println("====驗證成功====");
+                        } else {
+                            System.out.println("====驗證失敗====");
+                        }
+                    } else {
+                        System.out.println("====驗證失敗====");
+                    }
+                } else {
+                    System.out.println("====驗證失敗====");
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
