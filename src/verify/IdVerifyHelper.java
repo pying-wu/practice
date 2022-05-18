22edd4adeb516173849326878fe9309693ac1f64
sunny <sunnyright159@gmail.com>
2022-05-18 09:44:29 +0800

    amd2

--- src/verify/IdVerifyHelper.java
+++ src/verify/IdVerifyHelper.java
@@ -15,25 +15,28 @@ public class IdVerifyHelper {
         try {
             String pathname = "src/verify/idList.txt";
             File filename = new File(pathname);
-            BufferedReader br = new BufferedReader(new FileReader(String.valueOf(filename)));//构造一个BufferedReader类来读取文件
+            BufferedReader br = new BufferedReader(new FileReader(String.valueOf(filename)));//構造一个BufferedReader類来讀取文件
             String s = "";
             while ((s = br.readLine()) != null) {
                 System.out.println("====您輸入的身分證字號" + s + "====");
-                if (s.matches("[a-zA-Z]\\d{9}")) {
-                    s = s.toUpperCase();
-                    char letter = s.charAt(0); //取出第一個字母
-                    s = letterNums[letter - 'A'] + s.substring(1);
-                    int total = s.charAt(0) - '0';
-                    if (s.length() > 9) {
-                        for (int i = 1; i < 10; i++) {
-                            total += (s.charAt(i) - '0') * (10 - i); //依序加總
-                        }
-                        //以10減去加總值之個位數後取個位數
-                        int checkNum = (10 - total % 10) % 10;
-
-                        //計算結果和最後一位數比較
-                        if (checkNum == (s.charAt(10) - '0')) {
-                            System.out.println("====驗證成功====");
+                if (s.matches("[A-Z]\\d{9}")) {
+//                    s = s.toUpperCase();
+                    if (s.charAt(1) == '1' || s.charAt(1) == '2') {
+                        char letter = s.charAt(0); //取出第一個字母
+                        s = letterNums[letter - 'A'] + s.substring(1); //變成一整串數字
+                        int total = s.charAt(0) - '0';
+                        if (s.length() == 11) {
+                            for (int i = 1; i < 10; i++) {
+                                total += (s.charAt(i) - '0') * (10 - i); //依序加總
+                            }
+                            //以10減去加總值之個位數後取個位數
+                            int checkNum = (10 - total % 10) % 10;
+                            //計算結果和最後一位數比較
+                            if (checkNum == (s.charAt(10) - '0')) {
+                                System.out.println("====驗證成功====");
+                            } else {
+                                System.out.println("====驗證失敗====");
+                            }
                         } else {
                             System.out.println("====驗證失敗====");
                         }
