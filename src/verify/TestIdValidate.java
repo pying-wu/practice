87bd0af50036d86424bb77216655ee4bbd917d8e
sunny <sunnyright159@gmail.com>
2022-05-19 16:31:51 +0800

    modify TestIdValidate add VerifyHelperC01 and VerifyHelperC02

--- src/verify/TestIdValidate.java
+++ src/verify/TestIdValidate.java
@@ -6,9 +6,40 @@ public class TestIdValidate {
 
 	public static void main(String[] args) {
 		IdVerifyHelper helper = new IdVerifyHelper();
-		List<VerifyResult> resultList = helper.validate("src/verify/idList2.txt");
+		List<VerifyResult> resultList = helper.validate("src/verify/idList3.txt");
 
 		for (VerifyResult result : resultList) {
+			String id = result.getId();
+			boolean verifySucess = result.isVerifySuccess();
+			String message = result.getMessage();
+			System.out.println(id);
+			System.out.println(verifySucess);
+			System.out.println(message);
+		}
+
+		System.out.println("======  VerifyHelperC01  ======");
+		System.out.println("");
+
+		VerifyHelperC01 helper1 = new VerifyHelperC01();
+		List<VerifyResult> resultList1 = helper1.validate("src/verify/idList3.txt");
+
+		for (VerifyResult result : resultList1) {
+			String id = result.getId();
+			boolean verifySucess = result.isVerifySuccess();
+			String message = result.getMessage();
+			System.out.println(id);
+			System.out.println(verifySucess);
+			System.out.println(message);
+		}
+
+		System.out.println("");
+		System.out.println("====== VerifyHelperC02 開始 ======");
+		System.out.println("");
+
+		VerifyHelperC02 helper2 = new VerifyHelperC02();
+		List<VerifyResult> resultList2 = helper2.validate("src/verify/idList3.txt");
+
+		for (VerifyResult result : resultList2) {
 			String id = result.getId();
 			boolean verifySucess = result.isVerifySuccess();
 			String message = result.getMessage();
---
+++ src/verify/VerifyHelperC02.java
@@ -0,0 +1,71 @@
+package verify;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VerifyHelperC02 extends IdVerifyHelper {
+
+	@Override
+	public List<VerifyResult> validate(String filename) {
+		List<VerifyResult> resultList = new ArrayList<>();
+		int[] letterNums = { 10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32,
+				30, 31, 33 };
+		try {
+			File file = new File(filename);
+			BufferedReader br = new BufferedReader(new FileReader(String.valueOf(file)));
+			String s;
+			while ((s = br.readLine()) != null) {
+				VerifyResult vr = new VerifyResult();
+				vr.setId(s);
+				System.out.println("====您輸入的身分證字號 " + s + "====");
+				if (s.matches("^[a-z0-9A-Z\u4e00-\u9fa5]+$")) {
+					if (s.charAt(1) == '1' || s.charAt(1) == '2') {
+						char letter = s.toUpperCase().charAt(0);
+						s = letterNums[letter - 'A'] + s.substring(1);
+						int total = s.charAt(0) - '0';
+						if (s.length() == 11) {
+							for (int i = 1; i < 10; i++) {
+								total += (s.charAt(i) - '0') * (10 - i);
+							}
+							int checkNum = (10 - total % 10) % 10;
+							if (checkNum == (s.charAt(10) - '0')) {
+								System.out.println("====驗證成功====");
+								vr.setVerifySuccess(true);
+								vr.setMessage("驗證成功");
+								resultList.add(vr);
+							} else {
+								System.out.println("====驗證失敗====");
+								vr.setVerifySuccess(false);
+								vr.setMessage("驗證失敗");
+								resultList.add(vr);
+							}
+						} else {
+							System.out.println("====驗證失敗====");
+							vr.setVerifySuccess(false);
+							vr.setMessage("驗證失敗");
+							resultList.add(vr);
+						}
+					} else {
+						System.out.println("====驗證失敗====");
+						vr.setVerifySuccess(false);
+						vr.setMessage("驗證失敗");
+						resultList.add(vr);
+					}
+				} else {
+					System.out.println("====驗證失敗====");
+					vr.setVerifySuccess(false);
+					vr.setMessage("證號格式錯誤*");
+					resultList.add(vr);
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultList;
+	}
+
+}
